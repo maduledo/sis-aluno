68e6c0066cea2910c002d2fd977a1840109b6c22
maria eduarda lédo <145464361+maduledo@users.noreply.github.com>
2024-04-09 11:44:14 -0300

    Add files via upload

---
+++ sistema-aluno/testealuno.php
@@ -0,0 +1,15 @@
+
+    <?php
+var_dump($_GET);
+echo "<br>";
+echo "O nome é:" . $_GET['nome'];
+echo "<br>";
+echo "O endereço é:" . $_GET['endereco'];
+echo "<br>";
+echo "A data de nascimento digitada foi:" . $_GET['nasc'];
+echo "<br>";
+echo "A senha é:" . $_GET['senha'];
+echo "<br>";
+echo "O email é:" . $_GET['email'];
+?>
+
